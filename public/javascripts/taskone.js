ab52d870dd888ce944c87eb7c8eca149af74ceee
Coventry <coventry@Coventrys-MacBook-Pro.local>
2021-12-01 04:17:57 -0500

    initial commit

--- public/javascripts/taskone.js
+++ public/javascripts/taskone.js
@@ -11,3 +11,26 @@ let sessionData = {};
 
 const urlParams = new URLSearchParams(window.location.search);
 const subjectNum = urlParams.get('subject');
+let subjects = [];
+// fetch the subjectNumbers collection
+async function fetchSubject(){
+  let response = await fetch('/subjects');
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return await response.json().blob();
+}
+
+fetchSubject().then((blob) => {
+  for (let i in blob) {
+    let innerarray = [];
+    for (let j in blob[i]) {
+      innerarray.push(blob[i][j]);
+    }
+    subjects.push(innerarray);
+  }
+  console.log(subjects[0]);
+})
+.catch((e) =>
+  console.log(e)
+);
