488128cead221f1899bd15d04345af5462a7d2b3
Coventry <coventry@Coventrys-MacBook-Pro.local>
2021-12-06 09:17:37 -0500

    make it better

--- server.js
+++ server.js
@@ -105,16 +105,7 @@ MongoClient.connect(dbConnectionString,{ useNewUrlParser: true, useUnifiedTopolo
         .catch(error=>console.error(error));
     })
 
-    app.post('/single_subject',(req,res)=>{
-      console.log(req.body);
-      let o_id = new mongo.ObjectId(req.body.data.id);
-      let collection = db.collection("subjectNumbers");
-      collection.findOne({"_id": o_id})
-      .then(result=>{
-        res.json(result);
-      })
-      .catch(error => console.error(error));
-    })
+
 
 
     //POST
@@ -160,6 +151,24 @@ MongoClient.connect(dbConnectionString,{ useNewUrlParser: true, useUnifiedTopolo
           // res.redirect('/')
         })
         .catch(error => console.error(error));
+    })
+
+    app.post('/single_subject',(req,res)=>{
+      console.log(req.body);
+      let o_id = new mongo.ObjectId(req.body.data.id);
+      let collection = db.collection("subjectNumbers");
+      collection.findOne({"_id": o_id})
+      .then(result=>{
+        // res.send(result);
+      })
+      .catch(error => console.error(error));
+
+     //  collection.findOne({_id: o_id}, function(err,doc) {
+     //   if (err) { throw err; }
+     //   else {
+     //     console.log("Updated");
+     //   }
+     // });
     })
 
 
