8a5252b7188c4cf20a7f211d45b27c5997394978
Coventry <coventry@Coventrys-MacBook-Pro.local>
2021-12-08 17:21:29 -0500

    make it better

--- public/javascripts/tasktwo.js
+++ public/javascripts/tasktwo.js
@@ -105,6 +105,7 @@ function showInstruction(){
 }
 
 function makeVideoVisible(){
+  consoled.log("make the video visible");
   document.querySelector("#videoContainer").visibility = "visible";
 }
 
